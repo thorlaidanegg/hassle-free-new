a0c9742f0eb52ac2f7092b2d1a5109691cfaa687
RanakMedlr <ranak.shah@medlr.in>
2024-11-27 09:30:57 +0530

    chnages to title

--- app/layout.js
+++ app/layout.js
@@ -6,8 +6,8 @@ import { ThemeProvider } from "@/components/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ranak Shah",
-  description: "Made by Ranak",
+  title: "HassleFree",
+  description: "",
 };
 
 export default function RootLayout({ children }) {
